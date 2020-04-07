95fbfb091733047031f0ef171728f62cc8a89084
Ann-ly Tram <ann-ly.tram@etu.utc.fr>
2020-04-07 22:49:22 +0200

    Add preprocessing step pipelines

--- processing/pipeline_zoo.py
+++ processing/pipeline_zoo.py
@@ -2,11 +2,12 @@ from typing import Callable
 
 from data_gathering.query_param_mapping import QueryParamsToFetchersPipeline
 from data_gathering.statuses_fetch import FetchersToStatuses
-from processing.basic_pipelines import InputPipelineStep, OutputPipelineStep
+from processing.basic_pipelines import InputPipelineStep, OutputPipelineStep, SpreadPipelineStep
 from processing.sentiment_analysis.preprocessing_step import PreprocessingPipeline
 from processing.sentiment_analysis.sentiment_analysis import SentimentAnalysisPipeline
 from processing.utils.flatten_pipeline import FlattenPipeline
 from processing.utils.statuses_to_json_pipeline import StatusesToJsonPipeline
+from processing.utils.extract_tweet_from_json import ExtractTweetFromJson
 
 
 def get_sentiment_analysis_pipeline(callback: Callable):
@@ -16,6 +17,9 @@ def get_sentiment_analysis_pipeline(callback: Callable):
         .link(QueryParamsToFetchersPipeline('query_params_mapping', checkpointed=True)) \
         .link(FetchersToStatuses('fetch_data', checkpointed=True)) \
         .link(FlattenPipeline('flatten_data')) \
+        .link(StatusesToJsonPipeline('to_json'))\
+        .link(ExtractTweetFromJson('extract_tweet'))\
+        .link(SpreadPipelineStep())\
         .link(PreprocessingPipeline('preprocess', checkpointed=True)) \
         .link(SentimentAnalysisPipeline('sentiment', checkpointed=True)) \
         .link(OutputPipelineStep('output_sentiment', callback))
