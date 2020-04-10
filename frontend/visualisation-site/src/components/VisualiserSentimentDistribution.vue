5f100792505deddca04779458c88c3a3e40d9ec2
Ann-ly Tram <ann-ly.tram@etu.utc.fr>
2020-04-10 22:54:00 +0200

    Update VisualiserSentiment with RT filter

--- frontend/visualisation-site/src/components/VisualiserSentimentDistribution.vue
+++ frontend/visualisation-site/src/components/VisualiserSentimentDistribution.vue
@@ -8,6 +8,7 @@
         <v-text-field type="number" label="Since ID" v-model="since_id"></v-text-field>
         <v-text-field type="number" label="Max ID" v-model="max_id"></v-text-field>
         <v-slider style="margin-top: 32px;" v-model="count" min="5" max="100" label="Count" thumb-label="always"></v-slider>
+        <v-switch label="Filter retweets" v-model="filter_rt" value="True"></v-switch>
         <v-btn :loading="loading" @click="fetchData()">Fetch</v-btn>
         <ejs-chart id="container" title='sentiment distribution' v-if="data" :primaryXAxis='primaryXAxis'>
           <e-series-collection>
@@ -38,6 +39,7 @@
       until: '',
       since_id: undefined,
       max_id: undefined,
+      filter_rt : '',
       data: undefined,
       primaryXAxis: {
         valueType: 'Category',
@@ -49,7 +51,7 @@
       async fetchData() {
         this.loading = true;
         const nonTransformedData = await axios.get(
-                `http://127.0.0.1:5000/sentiment-distribution?query=${this.query}&count=${this.count}&until=${this.until}${this.since_id ? '&since_id=' + this.since_id : ''}${this.max_id ? '&max_id=' + this.max_id : ''}`
+                `http://127.0.0.1:5000/sentiment-distribution?query=${this.query}&filter_retweets=${this.filter_rt}&count=${this.count}&until=${this.until}${this.since_id ? '&since_id=' + this.since_id : ''}${this.max_id ? '&max_id=' + this.max_id : ''}`
         );
         const values = nonTransformedData.data.sentiment_distribution;
         const data = [];
