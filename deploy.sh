150be2439febd1465842a151493a6f1a218a4a93
Karel vanden Bussche <karelvandenbussche@hotmail.com>
2020-04-27 10:49:53 +0200

    Some changes to the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-
 # Frontend on firebase
 cd ./frontend/visualisation-site || exit
 firebase deploy
@@ -8,9 +7,8 @@ cd ../..
 docker build --tag karelvb/bigdgroup27 .
 docker push karelvb/bigdgroup27
 
-
 # Backend on virtual wall
-# shellcheck disable=SC2035
-#scp -r -v out.tar groep36@193.191.169.46:~
-
-ssh groep36@193.191.169.46 "sudo docker run --publish 80:80 --detach --name backend karelvb/bigdgroup27"
+ssh groep36@193.191.169.46 "sudo docker stop backend
+sudo docker rm backend
+sudo docker run --publish 80:80 --detach --name backend karelvb/bigdgroup27
+"
